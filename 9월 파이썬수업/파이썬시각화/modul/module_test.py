13c610c79d34944653ffadae947063d1a6ea4a59
shinwoejin <shinwoejin@naver.com>
2022-09-20 10:39:26 +0900

    cc

---
+++ 9월 파이썬수업/파이썬시각화/modul/module_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 모듈 만들기
+
+# In[2]:
+
+
+def add(num1, num2):
+    return num1 + num2
+
+
+# In[3]:
+
+
+def subtraction(num1, num2):
+    return num1 - num2
+
+
+# In[4]:
+
+
+def Multiplication(num1, num2):
+    return num1 * num2
+
+
+# In[5]:
+
+
+def division(num1, num2):
+    return num1 / num2
+
+
+# In[ ]:
+
+
+
+
